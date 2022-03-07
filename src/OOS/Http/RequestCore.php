e9a2ca58838f0c61d1f9c3f7a8e5416037726bac
洪宝同 <410536770@qq.com>
2022-03-07 09:05:33 +0800

    兼容8.0

--- src/OOS/Http/RequestCore.php
+++ src/OOS/Http/RequestCore.php
@@ -787,7 +787,7 @@ class RequestCore
         }
 
         // As long as this came back as a valid resource...
-        if (is_resource($curl_handle)) {
+        if (is_resource($curl_handle) || (is_object($curl_handle) && in_array(get_class($curl_handle), ['CurlHandle', 'Swoole\Curl\Handler', 'Swoole\Coroutine\Curl\Handle'], true))) {
             // Determine what's what.
             $header_size = curl_getinfo($curl_handle, CURLINFO_HEADER_SIZE);
             $this->response_headers = substr($this->response, 0, $header_size);
@@ -842,12 +842,13 @@ class RequestCore
         $this->response = curl_exec($curl_handle);
 
         if ($this->response === false) {
-            throw new RequestCore_Exception('cURL resource: ' . (string)$curl_handle . '; cURL error: ' . curl_error($curl_handle) . ' (' . curl_errno($curl_handle) . ')');
+            throw new RequestCore_Exception('cURL error: ' . curl_error($curl_handle) . ' (' . curl_errno($curl_handle) . ')');
         }
 
         $parsed_response = $this->process_response($curl_handle, $this->response);
 
         curl_close($curl_handle);
+        unset($curl_handle);
 
         if ($parse) {
             return $parsed_response;
